227d1a4c7fc6257926b78d53251c1a2eee855e6f
dengbin <dengbin@meicloud.com>
2018-11-26 10:25:35 +0800

    调整用户同步

--- user-brickie/src/main/java/com/midea/wcp/user/UserBrickie.java
+++ user-brickie/src/main/java/com/midea/wcp/user/UserBrickie.java
@@ -6,11 +6,13 @@ import com.midea.wcp.api.TokenButler;
 import com.midea.wcp.commons.Wechat;
 import com.midea.wcp.commons.message.UserSyncMessage;
 import com.midea.wcp.commons.model.User;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-//@RabbitListener(queues = "wechat-user")
+@RabbitListener(queues = "wechat-user")
 @Component
 public class UserBrickie {
 
@@ -23,7 +25,7 @@ public class UserBrickie {
         this.compositePersistence = compositePersistence;
     }
 
-    //    @RabbitHandler
+    @RabbitHandler
     public void persistence(UserSyncMessage message) throws IOException, InterruptedException {
         AccessToken accessToken = tokenButler.get(message.getAppId(), message.getAppSecret());
         String url = "https://api.weixin.qq.com/cgi-bin/user/info?" +
--- user-porter/src/main/java/com/midea/wcp/user/UserPorterController.java
+++ user-porter/src/main/java/com/midea/wcp/user/UserPorterController.java
@@ -6,6 +6,8 @@ import com.google.gson.JsonObject;
 import com.midea.wcp.api.AccessToken;
 import com.midea.wcp.api.TokenButler;
 import com.midea.wcp.commons.Wechat;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +23,11 @@ public class UserPorterController {
     private TokenButler tokenButler;
 
     private final UserPorterService userPorterService;
+    private final RabbitTemplate rabbitTemplate;
 
-    public UserPorterController(UserPorterService userPorterService) {
+    public UserPorterController(UserPorterService userPorterService, RabbitTemplate rabbitTemplate) {
         this.userPorterService = userPorterService;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @PostMapping
@@ -33,24 +37,26 @@ public class UserPorterController {
     }
 
     @GetMapping
-    public List<String> pull(@RequestParam String appId, @RequestParam String appSecret) throws IOException, InterruptedException {
-        List<String> openIds = new ArrayList<>();
+    public String pull(@RequestParam String appId, @RequestParam String appSecret) throws IOException, InterruptedException {
         AccessToken accessToken = tokenButler.get(appId, appSecret);
         String url = "https://api.weixin.qq.com/cgi-bin/user/get?" +
                 "access_token=" + accessToken.value() +
                 "&next_openid=";
         JsonObject usersJson = Wechat.getResponse(url);
-        JsonArray users = usersJson.get("data").getAsJsonObject().get("openid").getAsJsonArray();
-        users.iterator().forEachRemaining(user -> openIds.add(user.getAsString()));
+        send(usersJson.get("data").getAsJsonObject().get("openid").getAsJsonArray());
         String nextOpenId = usersJson.get("next_openid").getAsString();
         while (StringUtils.isEmpty(usersJson.get("next_openid").getAsString())) {
             url = "https://api.weixin.qq.com/cgi-bin/user/get?" +
                     "access_token=" + accessToken.value() +
                     "&next_openid=" + nextOpenId;
             usersJson = Wechat.getResponse(url);
-            users.iterator().forEachRemaining(user -> openIds.add(user.getAsString()));
+            send(usersJson.get("data").getAsJsonObject().get("openid").getAsJsonArray());
         }
 
-        return openIds;
+        return usersJson.get("next_openid").getAsString();
+    }
+
+    private void send(JsonArray users) {
+        users.iterator().forEachRemaining(user -> rabbitTemplate.convertAndSend(user.getAsString()));
     }
 }
